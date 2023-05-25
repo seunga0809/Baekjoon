a7f5025874c53c0e886c03ab07009968990d0cd0
seunga0809 <128767073+seunga0809@users.noreply.github.com>
2023-05-25 17:26:58 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+data=[]
+for i in range(1, n+1):
+    if n % i == 0:
+        data.append(i)
+        
+if k > (len(data)):
+    print(0)
+else:
+    print(data[k-1])
